10b1761fc55725bdef8b9ecd6ef45bde193346f0
Kingsley <kingobaino@gmail.com>
2020-10-28 23:52:35 +0100

    Add update method

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -22,5 +22,16 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @article.destroy
     redirect_to  articles_path
-  end  
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+  @article = Article.find(params[:id])
+  @article.update(article_params)
+
+  redirect_to article_path(@article)
+  end
 end
