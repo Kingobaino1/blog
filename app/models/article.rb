79fe5f2226f1e6643832773a87194c20cc17aadb
ricardo <ramn21@gmail.com>
2020-10-30 15:47:14 +0100

    fix destroy action

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-  has_many :taggings
+  has_many :taggings, dependent: :destroy
   has_many :tags, through: :taggings
   has_many :comments
   has_attached_file :image
